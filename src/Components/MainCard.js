9a383d1dce4c01518cecaf7b3aadf0f6c8ce655a
Ian F <ian.ferg36@gmail.com>
2022-05-21 19:20:27 -0600

    finished styling and added more destinations to data object array

--- src/Components/MainCard.js
+++ src/Components/MainCard.js
@@ -5,20 +5,25 @@ export default function Maincard(props) {
   return (
     <div className="card-container">
       <img className="place-pic" src={`/images/${props.item.imageUrl}`}></img>
-      <img
-        className="maps-logo"
-        src="/images/pinlogo.png"
-        alt="googlemaps pin logo"
-      />
-      <h3 className="location">{props.item.location}</h3>
-      <a className="maps-link" href={props.item.googleMapsUrl}>
-        View on Google Maps
-      </a>
-      <h1 className="title"> {props.item.title}</h1>
-      <h3 className="timeline">
-        {props.item.startDate} - {props.item.endDate}
-      </h3>
-      <p className="description">{props.item.description}</p>
+
+      <div className="info-container">
+        <div className="google-container">
+          <img
+            className="maps-logo"
+            src="/images/pinlogo.png"
+            alt="googlemaps pin logo"
+          />
+          <h3 className="location">{props.item.location}</h3>
+          <a className="maps-link" href={props.item.googleMapsUrl}>
+            View on Google Maps
+          </a>
+        </div>
+        <h1 className="title"> {props.item.title}</h1>
+        <h3 className="timeline">
+          {props.item.startDate} - {props.item.endDate}
+        </h3>
+        <p className="description">{props.item.description}</p>
+      </div>
     </div>
   );
 }
--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,6 @@
 export default [
   {
+    id: 1,
     title: "Amsterdam",
     location: "Netherlands",
     googleMapsUrl:
@@ -7,7 +8,31 @@ export default [
     startDate: "03 March, 2020",
     endDate: "09 March, 2020",
     description:
-      "Amsterdam is the capital and most populous city of the Netherlands, it's beautiful and surprisingly quiet if you know where to look.",
+      "Amsterdam is the most populated city in the Netherlands and is known for its beautiful canals, quirky architiecture, and lively night life. This is absolutely one of my favorite cities in Europe and can actually be rather peaceful if you know where to look.",
     imageUrl: "IMG_4606.jpg",
+  },
+  {
+    id: 2,
+    title: "Bruges",
+    location: "Belgium",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Bruges,+Belgium/data=!4m2!3m1!1s0x47c350d0c11e420d:0x1aa2f35ac8834df7?sa=X&ved=2ahUKEwiqiZP09vH3AhUCGDQIHWUoB_QQ8gF6BAgCEAE",
+    startDate: "09 March, 2020",
+    endDate: "12 March, 2020",
+    description:
+      "Bruges is the capital and largest city of the West Flanders province in the Flemish Region of Belgium. The town is Sprawling with beautiful medieval, cobblestone architecture. Though it rained the majority of the time I was there I had a wonderful time and enjoyed the wide selection of Belgian Beers.",
+    imageUrl: "IMG_4649 2.jpg",
+  },
+  {
+    id: 3,
+    title: "London",
+    location: "United Kingdom",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/London,+UK/@51.5287718,-0.2416812,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862",
+    startDate: "12 March, 2020",
+    endDate: "14 March, 2020",
+    description:
+      "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. I did not intend to come to London but had to once the travel ban was announced. It's an incredible and lovely city that I wish I was able to spend more time in. Visiting Westminster Abbey to this day was one of the most beautiful experiences of my life",
+    imageUrl: "IMG_4686.jpg",
   },
 ];
